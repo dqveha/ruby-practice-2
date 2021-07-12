7d1169d19e4a49b34cf27de7907582e223761ee7
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-12 14:53:16 -0700

    Update function to expand leetspeak transcription to multiple words
    Co-authored-by: Araceli Valdovinos <valdovinosaraceli50@gmail.com>

--- lib/1337speak.rb
+++ lib/1337speak.rb
@@ -8,13 +8,20 @@
 
 class String
   def leetspeak
-  edited_self = self.gsub("e", "3").gsub("o", "0").gsub("I", "1")
-  end_of_word = edited_self.length
-  without_first = edited_self[1..end_of_word].gsub("s", "z")
-  new_self = edited_self[0].concat(without_first)
+  array_self = self.split(" ")
+  new_array = []
+  array_self.each do |array_element|
+    edited_self = array_element.gsub("e", "3").gsub("o", "0").gsub("I", "1").gsub(/\W/, "")
+    end_of_word = edited_self.length
+    without_first = edited_self[1..end_of_word].gsub("s", "z")
+    new_self = edited_self[0].concat(without_first)
+    new_array.push(new_self)
+    end
+  new_array.join(" ")
   end
-end 
+end
 
+# split the array, loop through each array, push each into array, then join array into a string
 
 
 
